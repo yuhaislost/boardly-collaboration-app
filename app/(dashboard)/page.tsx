6e3ab1813dc2c96460762bbc061c436d9fbdc7ac
Yuhang Li <68719011+yuhaislost@users.noreply.github.com>
2024-04-03 22:51:03 +1100

    Bug Fix: Unable to get search params in production

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,16 +3,24 @@
 import { useOrganization } from "@clerk/nextjs";
 import { EmptyOrg } from "./_components/empty-org";
 import { BoardList } from "./_components/board-list";
+import { useSearchParams } from "next/navigation";
 
-interface DashboardProps {
-    searchParams: {
-        search?: string;
-        favourites?: string;
-    }
-};
+// interface DashboardProps {
+//     searchParams: {
+//         search?: string;
+//         favourites?: string;
+//     }
+// };
 
-const DashboardPage = ({ searchParams } : DashboardProps) => {
+const DashboardPage = () => {
     const { organization } = useOrganization();
+
+    const params  = useSearchParams();
+
+    let searchParams = {
+        search: params.get('search') || undefined,
+        favourites: params.get("favourites") || undefined
+    } as { search?: string; favourites?: string};
 
     return (
         <div className="flex-1 h-[calc(100%-80px)] p-6">
